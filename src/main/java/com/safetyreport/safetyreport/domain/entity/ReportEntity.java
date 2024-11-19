a3ffb27c863f1a5c13ad8c607f56033114cfedf5
woals2840 <woals2840@gmail.com>
2024-11-19 11:17:01 +0900

    [#3] fix(PhotoEntity): Entity수정

--- src/main/java/com/safetyreport/safetyreport/domain/entity/ReportEntity.java
+++ src/main/java/com/safetyreport/safetyreport/domain/entity/ReportEntity.java
@@ -1,10 +1,13 @@
 package com.safetyreport.safetyreport.domain.entity;
 
+import com.safetyreport.safetyreport.domain.Category;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.time.LocalDateTime;
 
@@ -26,22 +29,29 @@ public class ReportEntity {
     public String phoneNumber;
 
     @Enumerated(EnumType.STRING)
-    public String category;
+    public Category category = Category.PARKING;
 
     @Column(name = "created_at")
-    public LocalDateTime createdAt;
+    public LocalDateTime createdAt = LocalDateTime.now();
 
     @ManyToOne(targetEntity = UserEntity.class, fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private UserEntity userEntity;
 
+    @ManyToOne(targetEntity = PhotoEntity.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "photo_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private PhotoEntity photoEntity;
+
     @Builder
-    public ReportEntity(String address, String category, String content, LocalDateTime createdAt, Long id, String phoneNumber) {
+    public ReportEntity(String address, Category category, String content, LocalDateTime createdAt, Long id, String phoneNumber, UserEntity userEntity) {
         this.address = address;
         this.category = category;
         this.content = content;
         this.createdAt = createdAt;
         this.id = id;
         this.phoneNumber = phoneNumber;
+        this.userEntity = userEntity;
     }
 }
